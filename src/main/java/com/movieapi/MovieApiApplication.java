ef679e026ac7ecd2914f25b41052fc0c6b4e05c9
Ajay Kathwate <ajaykathwate13@gmail.com>
2024-10-05 23:59:26 +0530

    Configure application.yml file for DB and JPA

--- src/main/java/com/movieapi/MovieApiApplication.java
+++ src/main/java/com/movieapi/MovieApiApplication.java
@@ -1,10 +1,13 @@
 package com.movieapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieApiApplication {
+	@Value("${spring.datasource.username}")
+	private static String name;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApiApplication.class, args);
